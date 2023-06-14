1c439d25bf0fbb3c97b3acadb6f66f62f9bd8cdf
kirill vorobev <kirillvorobev728@gmail.com>
2023-06-14 22:50:07 +0300

    new files

---
+++ aplication/salary.py
@@ -0,0 +1,5 @@
+def calculate_salary(x, y):
+        y * x
+        return
+print(calculate_salary(2, 4))
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from Acco_ng.aplication.salary import calculate_salary
+from Acco_ng.aplication.db.people import get_employees
+if __name__ == '__main__':
+    print(calculate_salary)
+    print(get_employees)
